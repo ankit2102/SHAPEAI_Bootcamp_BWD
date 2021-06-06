e58fee14b89a52d258c11fb0720be83e45302640
ankit2102 <85442036+ankit2102@users.noreply.github.com>
2021-06-06 19:55:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+
+      <p>
+        This bootcamp was taken up by Shaurya Sinha Sir. He taught everything
+        about javascript HTML and react js from scratch.{" "}
+      </p>
+      <p>
+        It was a great experience. He taught us in easiest way possible. He is a
+        great personality.
+      </p>
+      <p>
+        Sir your passion for teaching and your dedication to your students is
+        marvelous
+      </p>
+    </div>
+  );
+}
+
+export default Note;
